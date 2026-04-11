cc82b6471642c6e5d2ec688a1073efc189839cdc
Sonryu <watsonvilar@gmail.com>
2026-04-11 17:36:49 -0300

    questao 2

--- questao2.c
+++ questao2.c
@@ -4,6 +4,6 @@ int main(void){
 
     int i=5, *p;
     p = &i;
-    printf("%p %p %d %d %d %d\n", p, p+1, *p+2, **&p, 3**p, **&p+4);
+    printf("%p\n %p\n %d\n %d\n %d\n %d\n", p, p+1, *p+2, **&p, 3**p, **&p+4);
 
 }
