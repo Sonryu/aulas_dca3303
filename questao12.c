00a3ec22c11d9496a45a5df7c1f46ab9f0f178a1
Sonryu <watsonvilar@gmail.com>
2026-04-12 08:10:44 -0300

    questao 12

---
+++ questao12.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+    void main(){ // void main() eh uma pratica obsoleta, atualmente o certo eh usar int main para ter o retorno de um numero da funcao main
+    int const *x = 3; // o ponteiro x esta apontando para o ENDERECO 3 DA MEMORIA
+    printf("%d", ++(*x)); // *x foi declarado como uma constante, esta protegido e n pode ser mudado
+    
+    }
+
+*/
+
+int main(){
+
+printf("LER OS COMENTARIOS DO CODIGO." );
+
+
+}
